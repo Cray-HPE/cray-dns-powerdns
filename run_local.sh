7cb19f560b4b4dab0dd12dfe355a8b90f8bdc7ef
Chris Spiller <christian.spiller@hpe.com>
2021-11-30 19:16:38 +0000

    CASMNET-859 - Move port as non-privileged user can't bind to port 53

--- run_local.sh
+++ run_local.sh
@@ -10,4 +10,4 @@ docker rmi -f powerdns-test
 
 
 docker build -t powerdns-test -f Dockerfile .
-docker run --name powerdns-test -p 53:53 -p 53:53/udp -e AUTOCONF=sqlite docker.io/library/powerdns-test --cache-ttl=120 --allow-axfr-ips=127.0.0.1,123.1.2.3
+docker run --name powerdns-test -p 53:5053 -p 53:5053/udp -e AUTOCONF=sqlite docker.io/library/powerdns-test --cache-ttl=120 --allow-axfr-ips=127.0.0.1,123.1.2.3
